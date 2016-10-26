411fc543d58c098e4b3eff9d1d77cbd153f7c70a
ronaldcarucci <ronaldcarucci@hotmail.be>
2016-10-26 09:01:32 +0200

    Creation of Grid Class Test Program with correct, incorrect and uncompleted Grid

--- ConsoleSudokuResolver/Program.cs
+++ ConsoleSudokuResolver/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibrarySudokuResolver;
 
 namespace ConsoleSudokuResolver
 {
@@ -10,6 +11,53 @@ namespace ConsoleSudokuResolver
     {
         static void Main(string[] args)
         {
+            int[,] myGrid = {   // GRID TEST WITHOUT ERROR
+                                {5, 6, 7, 2, 4, 9, 3, 8, 1 },
+                                {8, 4, 3 ,1 ,5 ,7, 2, 6, 9 },
+                                {1, 2, 9, 6, 3, 8, 5, 4, 7 },
+                                {2, 7, 4, 3, 9, 1, 6, 5, 8 },
+                                {3, 8, 1, 5, 7, 6, 4, 9, 2 },
+                                {9, 5, 6, 8, 2, 4, 7, 1, 3 },
+                                {4, 3, 8, 7, 1, 5, 9, 2, 6 },
+                                {7, 1, 5, 9, 6, 2, 8, 3, 4 },
+                                {6, 9, 2, 4, 8, 3, 1 ,7, 5 }
+                            };
+
+            Grid grid = new Grid (myGrid);
+
+            Console.WriteLine("RESOLVED ? {0}", grid.CheckGrid());
+
+            int[,] myGrid2 = {  // GRID TEST WITH ERROR IN [8,7] AND [8,8] ( 5 <=> 7 )
+                                {5, 6, 7, 2, 4, 9, 3, 8, 1 },
+                                {8, 4, 3 ,1 ,5 ,7, 2, 6, 9 },
+                                {1, 2, 9, 6, 3, 8, 5, 4, 7 },
+                                {2, 7, 4, 3, 9, 1, 6, 5, 8 },
+                                {3, 8, 1, 5, 7, 6, 4, 9, 2 },
+                                {9, 5, 6, 8, 2, 4, 7, 1, 3 },
+                                {4, 3, 8, 7, 1, 5, 9, 2, 6 },
+                                {7, 1, 5, 9, 6, 2, 8, 3, 4 },
+                                {6, 9, 2, 4, 8, 3, 1 ,5, 7 }
+                            };
+
+            Grid grid2 = new Grid(myGrid2);
+
+            Console.WriteLine("RESOLVED ? {0}", grid2.CheckGrid());
+
+            int[,] myGrid3 = {  // GRID TEST UNCOMPLETED ([3,7],[5,1],[8,3] EMPTY (SETTED TO 0))
+                                {5, 6, 7, 2, 4, 9, 3, 8, 1 },
+                                {8, 4, 3 ,1 ,5 ,7, 2, 6, 9 },
+                                {1, 2, 9, 6, 3, 8, 5, 4, 7 },
+                                {2, 7, 4, 3, 9, 1, 6, 0, 8 },
+                                {3, 8, 1, 5, 7, 6, 4, 9, 2 },
+                                {9, 0, 6, 8, 2, 4, 7, 1, 3 },
+                                {4, 3, 8, 7, 1, 5, 9, 2, 6 },
+                                {7, 1, 5, 9, 6, 2, 8, 3, 4 },
+                                {6, 9, 2, 0, 8, 3, 1 ,5, 7 }
+                            };
+
+            Grid grid3 = new Grid(myGrid3);
+
+            Console.WriteLine("RESOLVED ? {0}", grid3.CheckGrid());
         }
     }
 }
---
+++ LibrarySudokuResolver/Grid.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySudokuResolver
+{
+    public class Grid
+    {
+        public int[,] Values { get; private set; }
+
+        public int MinValue { get; set; }
+
+        public int MaxValue { get; set; }
+
+        public int SumToHave { get; set; }
+
+        public int Lines { get; private set; }
+
+        public int Columns { get; private set; }
+
+        public Grid(int lines = 9, int columns = 9, int minValue = 1, int maxValue = 9, int sumToHave = 49)
+        {
+            if (lines <= 0 || columns <= 0)
+            {
+                // ERROR
+            }
+            else
+            {
+                Lines = lines;
+                Columns = columns;
+
+                Values = new int[Lines,Columns];
+
+                MinValue = minValue;
+                MaxValue = maxValue;
+                SumToHave = sumToHave;
+            }
+        }
+
+        public Grid(int[,] values, int minValue = 1, int maxValue = 9, int sumToHave = 45)
+        {
+            if (values == null)
+            {
+                // ERROR
+            }
+            else
+            {
+                Lines = values.GetLength(0);
+                Columns = values.GetLength(0);
+                Values = new int[Lines, Columns];
+
+                for (int i = 0 ; i < Lines ; i++)
+                    for (int j = 0; j < Columns; j++)
+                        Values[i, j] = values[i, j];
+
+                MinValue = minValue;
+                MaxValue = maxValue;
+                SumToHave = sumToHave;
+            }
+        }
+
+        public bool CheckGrid()
+        {
+            
+
+
+            // Check each Line
+            for (int i = 0 ; i < Lines ; i++)
+            {
+                int tmpSum = 0;
+
+                for (int j = 0 ; j < Columns ; j++)
+                    tmpSum += Values[i,j];
+
+                if (tmpSum != SumToHave)
+                    return false;
+            }
+
+            // Check each Column
+            for (int i = 0; i < Columns ; i++)
+            {
+                int tmpSum = 0;
+
+                for (int j = 0; j < Lines ; j++)
+                    tmpSum += Values[j, i];
+
+                if (tmpSum != SumToHave)
+                    return false;
+            }
+
+            // Check each 9-case Square
+            for (int k = 0; k < 3; k++)
+                for (int l = 0; l < 3; l++)
+                {
+                    int tmpSum = 0;
+
+                    for (int i = (0 + (3 * k)); i < (3 + (3 * k)); i++)
+                        for (int j = (0 + (3 * l)); j < (3 + (3 * l)); j++)
+                            tmpSum += Values[i, j];
+
+                    if (tmpSum != SumToHave)
+                        return false;
+                }
+
+            return true;
+            
+        }
+        
+    }
+}
