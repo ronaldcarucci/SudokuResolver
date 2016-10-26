7c51e8263c3ba0305aa56caf83c36e586b7fdc1a
ronaldcarucci <ronaldcarucci@hotmail.be>
2016-10-26 14:05:24 +0200

    Resolver Complete but too slow ! It take so time to resolve a grid.

--- ConsoleSudokuResolver/Program.cs
+++ ConsoleSudokuResolver/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleSudokuResolver
 {
     class Program
     {
+        static Grid grid2;
         static void Main(string[] args)
         {
             int[,] myGrid = {   // GRID TEST WITHOUT ERROR
@@ -25,39 +26,61 @@ namespace ConsoleSudokuResolver
 
             Grid grid = new Grid (myGrid);
 
-            Console.WriteLine("RESOLVED ? {0}", grid.CheckGrid());
+            int[,] temp = grid.Values;
 
-            int[,] myGrid2 = {  // GRID TEST WITH ERROR IN [8,7] AND [8,8] ( 5 <=> 7 )
-                                {5, 6, 7, 2, 4, 9, 3, 8, 1 },
-                                {8, 4, 3 ,1 ,5 ,7, 2, 6, 9 },
-                                {1, 2, 9, 6, 3, 8, 5, 4, 7 },
-                                {2, 7, 4, 3, 9, 1, 6, 5, 8 },
-                                {3, 8, 1, 5, 7, 6, 4, 9, 2 },
-                                {9, 5, 6, 8, 2, 4, 7, 1, 3 },
-                                {4, 3, 8, 7, 1, 5, 9, 2, 6 },
-                                {7, 1, 5, 9, 6, 2, 8, 3, 4 },
-                                {6, 9, 2, 4, 8, 3, 1 ,5, 7 }
+            Console.WriteLine("Grid 1 : ");
+            PrintGrid(grid);
+            
+            int[,] myGrid2 = {  // GRID TEST UNCOMPLETED
+                                {5,3,0,0,7,0,0,0,0},
+                                {6,0,0,1,9,5,0,0,0},
+                                {0,9,8,0,0,0,0,6,0},
+                                {8,0,0,0,6,0,0,0,3},
+                                {4,0,0,8,0,3,0,0,1},
+                                {7,0,0,0,2,0,0,0,6},
+                                {0,6,0,0,0,0,2,8,0},
+                                {0,0,0,4,1,9,0,0,5},
+                                {0,0,0,0,8,0,0,7,9}
                             };
 
-            Grid grid2 = new Grid(myGrid2);
+            grid2 = new Grid(myGrid2);
 
-            Console.WriteLine("RESOLVED ? {0}", grid2.CheckGrid());
+            Console.WriteLine("Grid 2 Uncompleted : ");
+            PrintGrid(grid2);
 
-            int[,] myGrid3 = {  // GRID TEST UNCOMPLETED ([3,7],[5,1],[8,3] EMPTY (SETTED TO 0))
-                                {5, 6, 7, 2, 4, 9, 3, 8, 1 },
-                                {8, 4, 3 ,1 ,5 ,7, 2, 6, 9 },
-                                {1, 2, 9, 6, 3, 8, 5, 4, 7 },
-                                {2, 7, 4, 3, 9, 1, 6, 0, 8 },
-                                {3, 8, 1, 5, 7, 6, 4, 9, 2 },
-                                {9, 0, 6, 8, 2, 4, 7, 1, 3 },
-                                {4, 3, 8, 7, 1, 5, 9, 2, 6 },
-                                {7, 1, 5, 9, 6, 2, 8, 3, 4 },
-                                {6, 9, 2, 0, 8, 3, 1 ,5, 7 }
-                            };
 
-            Grid grid3 = new Grid(myGrid3);
+            Task T1 = new Task(AlwaysPrint);
+            T1.Start();
 
-            Console.WriteLine("RESOLVED ? {0}", grid3.CheckGrid());
+            grid2.AutoSolve();
+
+            Console.WriteLine("Grid 2 Completed : ");
+            PrintGrid(grid2);
+
+        }
+
+        static void PrintGrid(Grid grid)
+        {
+            for (int i = 0; i < grid.Lines; i++)
+            {
+                for (int j = 0; j < grid.Columns; j++)
+                    Console.Write("{0} ", grid.Values[i, j]);
+                Console.WriteLine();
+            }
+        }
+        
+        static void AlwaysPrint()
+        {
+            int i = 0;
+            while (grid2.CheckGrid() == false)
+            {
+                if (i % 1500 == 0)
+                {
+                    Console.Clear();
+                    PrintGrid(grid2);
+                }
+                i++;
+            }
         }
     }
 }
--- LibrarySudokuResolver/Grid.cs
+++ LibrarySudokuResolver/Grid.cs
@@ -8,18 +8,44 @@ namespace LibrarySudokuResolver
 {
     public class Grid
     {
+        /// <summary>
+        /// The multidimensional array containing all the Values of the grid.
+        /// </summary>
         public int[,] Values { get; private set; }
 
+        /// <summary>
+        /// The minimal value accepted in the grid.
+        /// </summary>
         public int MinValue { get; set; }
 
+        /// <summary>
+        /// The maximal value accepted in the grid.
+        /// </summary>
         public int MaxValue { get; set; }
 
+        /// <summary>
+        /// The total of each test.
+        /// </summary>
         public int SumToHave { get; set; }
 
+        /// <summary>
+        /// The number of Lines.
+        /// </summary>
         public int Lines { get; private set; }
 
+        /// <summary>
+        /// The number of Columns.
+        /// </summary>
         public int Columns { get; private set; }
 
+        /// <summary>
+        /// Contruct an empty Sudoku Grid 
+        /// </summary>
+        /// <param name="lines">Number of Lines</param>
+        /// <param name="columns">Number of Columns</param>
+        /// <param name="minValue">Accepted Minimal Value</param>
+        /// <param name="maxValue">Accepted Maximal Value</param>
+        /// <param name="sumToHave">Total to have for each Test</param>
         public Grid(int lines = 9, int columns = 9, int minValue = 1, int maxValue = 9, int sumToHave = 49)
         {
             if (lines <= 0 || columns <= 0)
@@ -39,6 +65,13 @@ namespace LibrarySudokuResolver
             }
         }
 
+        /// <summary>
+        /// Contruct a Sudoky Grid from a Array
+        /// </summary>
+        /// <param name="values">Array containing values.</param>
+        /// <param name="minValue">Accepted Minimal Value</param>
+        /// <param name="maxValue">Accepted Maximal Value</param>
+        /// <param name="sumToHave">Total to have for each Test</param>
         public Grid(int[,] values, int minValue = 1, int maxValue = 9, int sumToHave = 45)
         {
             if (values == null)
@@ -61,11 +94,12 @@ namespace LibrarySudokuResolver
             }
         }
 
+        /// <summary>
+        /// Check if the grid is completed and valid
+        /// </summary>
+        /// <returns>True/False</returns>
         public bool CheckGrid()
         {
-            
-
-
             // Check each Line
             for (int i = 0 ; i < Lines ; i++)
             {
@@ -105,8 +139,85 @@ namespace LibrarySudokuResolver
                 }
 
             return true;
-            
+        }
+
+        /// <summary>
+        /// Solve automaticaly the grid
+        /// </summary>
+        public void AutoSolve()
+        {
+            List<int[,]> zeros = SearchZeros();
+
+            foreach (int[,] zero in zeros)
+                Values[zero[0, 0], zero[0, 1]] = 1;
+
+            ProcessAutoSolve(zeros);
+        }
+
+        /// <summary>
+        /// Search the next zero-value in the grid
+        /// </summary>
+        /// <param name="fromLine">beginning index</param>
+        /// <param name="fromColumn">endinf index</param>
+        /// <returns>The position of the next zero-value</returns>
+        public int[,] NextZero(int fromLine = 0, int fromColumn = 0)
+        {
+            int[,] Nullvalue = { { -1, -1 } };
+
+            for (int i = fromLine; i < Lines; i++)
+                for (int j = fromColumn; j < Columns; j++)
+                    if (Values[i, j] == 0)
+                    {
+                        int[,] value = { { i, j } };
+                        return value;
+                    }
+            return Nullvalue;
+        }
+
+        /// <summary>
+        /// Search every zero-values
+        /// </summary>
+        /// <returns>The list of zero-value</returns>
+        public List<int[,]> SearchZeros()
+        {
+            List<int[,]> result = new List<int[,]>();
+
+            int[,] zero = { { -1, -1 } };
+
+            for (int i = 0; i < Lines; i++)
+                for (int j = 0; j < Columns; j++)
+                    if (Values[i, j] == 0)
+                    {
+                        int[,] value = { { i, j } };
+                        result.Add(value);
+                    }
+
+            return result;
         }
         
+        private void ProcessAutoSolve(List<int[,]> zeros, int current = 0)
+        {
+            while (CheckGrid() == false)
+            {
+                Increase(zeros);
+            }
+        }
+
+        private void CancelAutoSolve(List<int[,]> zeros)
+        {
+            foreach (int[,] zero in zeros)
+                Values[zero[0, 0], zero[0, 1]] = 0;
+        }
+
+        private void Increase(List<int[,]> zeros, int index = 0)
+        {
+            Values[zeros[index][0, 0], zeros[index][0, 1]]++;
+            if (Values[zeros[index][0, 0], zeros[index][0, 1]] == 10)
+            {
+                Values[zeros[index][0, 0], zeros[index][0, 1]] = 1;
+                if (index < zeros.Count - 1)
+                    Increase(zeros, index + 1);
+            }
+        }
     }
 }
